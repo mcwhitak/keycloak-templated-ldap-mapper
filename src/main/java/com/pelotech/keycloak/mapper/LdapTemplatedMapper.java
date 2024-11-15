e2284cd12f298b21c1b7b86f4233367442b5d729
Michael Whitaker <mcwhitaker@fastmail.com>
2024-11-15 09:04:21 -0600

    feat: fix formatting

--- src/main/java/com/pelotech/keycloak/mapper/LdapTemplatedMapper.java
+++ src/main/java/com/pelotech/keycloak/mapper/LdapTemplatedMapper.java
@@ -6,8 +6,6 @@ import freemarker.template.TemplateException;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
